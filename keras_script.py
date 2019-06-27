2a0e1a180e060848da58a349fddac431492d8709
Ankit Mishra <ankitmishra723@gmail.com>
2019-06-27 08:07:07 +0530

    labels added

--- keras_script.py
+++ keras_script.py
@@ -1,7 +1,18 @@
 import numpy as np
 
 from keras.models import Sequential
-from my_classes import DataGenerator
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import os
+path = 'D:/myProject/DeepLearningModels/'
+os.chdir(path)
+
+
+
+df_data = pd.read_csv(path+'map_traininglabels.csv')
+X_train, X_test, y_train, y_test = train_test_split(df_data['image_id'], df_data['Class'], test_size=0.25)
+
 
 # Parameters
 params = {'dim': (32,32,32),
@@ -11,8 +22,10 @@ params = {'dim': (32,32,32),
           'shuffle': True}
 
 # Datasets
-partition = # IDs
-labels = # Labels
+partition = {'train':X_train, 'validation':X_test}
+df_=df_data
+df_=df_.drop(['score'],1)
+labels = df_.set_index('image_id').T.to_dict('list')
 
 # Generators
 training_generator = DataGenerator(partition['train'], labels, **params)
