5cfcc2fbd438d06314f8520490787c1cdef52a62
Ankit Mishra <ankitmishra723@gmail.com>
2019-08-16 18:52:14 +0530

    gan_test_code

--- GANs/rewriting_GAN_mnist.py
+++ GANs/rewriting_GAN_mnist.py
@@ -14,7 +14,26 @@ plt.imshow(train_images[0,:].reshape(28,28))    # plotting 1st image, resizing 7
 
 tf.reset_default_graph() # resets default global graph
 
+'''
+a = tf.constant(5)
+b = tf.constant(10)
+c = tf.placeholder(tf.int32)
+z = a+b+c
+result = tf.Session().run(z, {c: 2})
+print(result)
+'''
+
+real_images = tf.placeholder(tf.float32, shape=[None, 784])  # size of image 28*28=784
+z = tf.placeholder(tf.float32, shape=[None, 100])            # planning to keep batch size = 100
 
 
+def generator(z, reuse=None):
+    with tf.variable_scope('gen', reuse=reuse):
+        hidden1 = tf.layers.dense(inputs=z, units=128, activation=tf.nn.leaky_relu)
+        hidden2 = tf.layers.dense(inputs=hidden1, units=128, activation=tf.nn.leaky_relu)
+        output = tf.layers.dense(inputs=hidden2, units=784, activation=tf.nn.tanh)
+        return output
 
 
+G = generator(z)
+.
